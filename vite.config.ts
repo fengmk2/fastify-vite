b4e930835ecd5550d67fd539fc2aeb3a514d5860
MK <fengmk2@gmail.com>
2026-01-08 20:38:38 +0800

    FIXUP

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from '@voidzero-dev/vite-plus';
+import { defineConfig } from '@voidzero-dev/vite-plus'
 
 export default defineConfig({
   fmt: {
-    "$schema": "./node_modules/oxfmt/configuration_schema.json",
-    "semi": false,
-    "singleQuote": true
+    $schema: './node_modules/oxfmt/configuration_schema.json',
+    semi: false,
+    singleQuote: true,
   },
   lint: {
-    "$schema": "./node_modules/oxlint/configuration_schema.json",
-    "categories": {
-      "correctness": "error"
+    $schema: './node_modules/oxlint/configuration_schema.json',
+    categories: {
+      correctness: 'error',
     },
-    "rules": {
-      "no-unused-vars": "warn"
-    }
+    rules: {
+      'no-unused-vars': 'warn',
+    },
   },
-});
+})
